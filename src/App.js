ff53f97480c5f5b3b0a063476ae6af34085aa0ad
Mayank <mayankyadav57476@gmail.com>
2023-10-26 15:18:29 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Header from './components/header';
+import Form from './components/meme';
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Form/>
+    </div>
+  );
+}
+
+export default App;
